135078e85af086dee8ea49fcc6abb6bc46607f62
Andres Segura <dev-andr7st@outlook.com>
2021-12-11 03:42:14 -0500

    Updates

---
+++ component---src-pages-404-js-f3d230611d423bc12e03.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_hello_world=self.webpackChunkgatsby_starter_hello_world||[]).push([[883],{9616:function(e,t,r){r.r(t);var l=r(7294);t.default=function(){return l.createElement("div",{class:"raiz"},l.createElement("div",{class:"first"},l.createElement("h3",null,"Andr7st"),l.createElement("h1",null,"Error 404"),l.createElement("h3",null,"This page does not exist"),l.createElement("a",{href:"#"},"54")))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-f3d230611d423bc12e03.js.map
